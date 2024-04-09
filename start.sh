a5681cbc7262a0db25f0af9b70b0214fd32563bf
morty000 <41410719+morty000@users.noreply.github.com>
2024-04-09 17:26:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,14 +15,14 @@ do
 	sleep 5
 done
 
-echo ${MYSQL_ROOT_PASSWORD} > /pwd
-echo ${IP} > /ip
+echo ${MYSQL_ROOT_PASSWORD} > /tmp/pwd
+echo ${IP} > /tmp/ip
 
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS maplestory_079"
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "use maplestory_079;source /MapleStory-Server-079/ms_20210813_234816.sql;"
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "use maplestory_079;source /usr/local/MapleStory-Server-079/ms_20210813_234816.sql;"
 
 
-sed -i "s/afauria/${MYSQL_ROOT_PASSWORD}/g"  /MapleStory-Server-079/config/db.properties
-sed -i "s/127.0.0.1/${IP}/g" /MapleStory-Server-079/config/server.properties
+sed -i "s/afauria/${MYSQL_ROOT_PASSWORD}/g"  /usr/local/MapleStory-Server-079/config/db.properties
+sed -i "s/127.0.0.1/${IP}/g" /usr/local/MapleStory-Server-079/config/server.properties
 
-nohup java -cp /MapleStory-Server-079/bin/maple.jar -server -DhomePath=/MapleStory-Server-079/config/ -DscriptsPath=/MapleStory-Server-079/scripts/ -DwzPath=/MapleStory-Server-079/scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start  > /MapleStory-Server-079/maple.log &
+nohup java -cp /usr/local/MapleStory-Server-079/bin/maple.jar -server -DhomePath=/usr/local/MapleStory-Server-079/config/ -DscriptsPath=/usr/local/MapleStory-Server-079/scripts/ -DwzPath=/usr/local/MapleStory-Server-079/scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start  > /usr/local/MapleStory-Server-079/logs/maple.log &
