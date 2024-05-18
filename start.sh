48d247921e02334c534dbaa71ade4dbe2e0b87f4
Ricky-Hao <14084342+Ricky-Hao@users.noreply.github.com>
2024-05-18 20:18:08 +0800

    Update docker

--- start.sh
+++ start.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
+WAIT_TIME=10
+
+while true; do
+    echo "Try to connect mysql."
+    mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$MYSQL_DATABASE" -e "SELECT 1;" &>/dev/null
+    if [ $? -eq 0 ]; then
+        echo "Mysql online!"
+        break
+    else
+        echo "Mysql offline."
+        sleep $WAIT_TIME
+    fi
+done
+
 
 TABLE_COUNT=$(mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}" -se"SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '${MYSQL_DATABASE}';")
 
 if [ "$TABLE_COUNT" -eq 0 ]; then
     echo "No tables found in '${MYSQL_DATABASE}' database. Starting data import..."
-    mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "use ${MYSQL_DATABASE};source /app/ms_20210813_234816.sql;"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "use ${MYSQL_DATABASE};source /app/ms_20210813_234816.sql;"
 
 else
     echo "Database contains tables. No need for data import."
@@ -16,4 +30,4 @@ sed -i "s/mysql_password/${MYSQL_PASSWORD}/g"  /app/config/db.properties
 sed -i "s/mysql_database/${MYSQL_DATABASE}/g"  /app/config/db.properties
 sed -i "s/public_ip/${PUBLIC_IP}/g" /app/config/server.properties
 
-java -cp /MapleStory_Server.jar -server -DhomePath=/app/config/ -DscriptsPath=/app/scripts/ -DwzPath=/app/scripts/wz/ -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
+java -jar /app/MapleStory_Server.jar -DhomePath=/app/config/ -DscriptsPath=/app/scripts/ -DwzPath=/app/scripts/wz/ -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
